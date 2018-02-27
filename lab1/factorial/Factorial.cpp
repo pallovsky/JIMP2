66964570342e6710de33135744c47112cab3f42e
pallovsky <pallovskyu@gmail.com>
2018-02-27 10:53:01 +0100

    factorial

--- lab1/factorial/Factorial.cpp
+++ lab1/factorial/Factorial.cpp
@@ -4,5 +4,21 @@
 #include "Factorial.h"
 
 int factorial(int value) {
-  return 0;
+
+    if (value == 0 || value == 1) {
+        return 1;
+    }
+
+    if (value > 12 || value < -12) {
+        return 0;
+    }
+
+    if (value > 0) {
+        return value * factorial(value - 1);
+    }
+
+    if (value < 0) {
+        return value * factorial(value + 1);
+    }
 }
+
